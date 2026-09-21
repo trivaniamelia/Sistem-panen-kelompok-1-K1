a425fef9a6e8ccbd0d491197b939990f79e144e3
agelufiyamal <agelufiyamal@gmail.com>
2026-09-21 19:59:34 +0700

    Delete diskon.py

--- diskon.py
+++
@@ -1,5 +0,0 @@
-def hitung_diskon(total_harga, persentase_diskon):
-    nilai_diskon = total_harga * persentase_diskon / 100
-    total_bayar = total_harga - nilai_diskon
-
-    return nilai_diskon, total_bayar
